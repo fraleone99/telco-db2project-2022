c982ef24a22e3520701033ba4456efa388af3a99
Lorenzo <lorenzo.iovine@mail.polimi.it>
2022-03-13 22:45:04 +0100

    Employee

---
+++ src/main/java/it/polimi/telcodb2project2022/services/EmployeeService.java
@@ -0,0 +1,29 @@
+package it.polimi.telcodb2project2022.services;
+
+import it.polimi.telcodb2project2022.entities.Employee;
+import it.polimi.telcodb2project2022.entities.User;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+@Stateless
+public class EmployeeService {
+    @PersistenceContext(unitName = "telcoEJB")
+    private EntityManager em;
+
+    public EmployeeService(){
+    }
+
+    public Employee insertEmployee(String username, String password) throws PersistenceException, IllegalArgumentException{
+        User user = new User();
+        Employee employee = new Employee();
+        employee.setPassword(password);
+        employee.setUsername(username);
+
+        em.persist(user);
+        return employee;
+    }
+
+}
---
+++ src/main/java/it/polimi/telcodb2project2022/servlets/SignUpEmployee.java
@@ -0,0 +1,92 @@
+package it.polimi.telcodb2project2022.servlets;
+
+import it.polimi.telcodb2project2022.entities.Employee;
+import it.polimi.telcodb2project2022.entities.User;
+import it.polimi.telcodb2project2022.exceptions.CredentialsException;
+import it.polimi.telcodb2project2022.services.EmployeeService;
+import it.polimi.telcodb2project2022.services.UserService;
+import org.apache.commons.text.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.persistence.PersistenceException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/SignUpEmployee")
+public class SignUpEmployee extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+    @EJB(name = "it.polimi.telcodb2project2022.services/EmployeeService")
+    private EmployeeService employeeService;
+
+    public SignUpEmployee() {
+        super();
+    }
+
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String path = "/registration.html";
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+        templateEngine.process(path, ctx, response.getWriter());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String usrn;
+        String pwd;
+        try {
+            usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
+            pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
+            if (usrn == null || pwd == null || usrn.isEmpty() || pwd.isEmpty()) {
+                throw new Exception("Missing or empty credential value");
+            }
+
+        } catch (Exception e) {
+            // for debugging only e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credential value");
+            return;
+        }
+
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+        try {
+            Employee employee = employeeService.insertEmployee(usrn, pwd);
+            String path = getServletContext().getContextPath() + "/index.html";
+            response.sendRedirect(path);
+        }
+        catch (PersistenceException | IllegalArgumentException | EJBException e) {
+            if (e.getCause().getCause().getMessage().contains("Duplicate entry")) {
+                ctx.setVariable("errorMsg", "Username already taken");
+                String path = "/registration.html";
+                templateEngine.process(path, ctx, response.getWriter());
+            }
+            else {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "internal server error");
+            }
+        }
+
+    }
+
+    public void destroy() {
+    }
+}
